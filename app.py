b2eb212e89556b09afc55f853a1981707576ce06
Shreyash <shreyashgarudkar1422@gmail.com>
2025-12-03 11:23:44 +0530

    python flask app

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+def create_app():
+
+    # create flask app
+    app = Flask(__name__)
+
+    # define routes
+
+    @app.route('/', methods=['GET'])
+    def root():
+        return "welcome to heaven"
+    
+    @app.route('/health', methods=['GET'])
+    def health():
+        return "healthy"
+    
+    return app
